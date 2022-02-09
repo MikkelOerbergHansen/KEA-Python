2dbae6676bd055ec1b53b90f49910702a7fcd285
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-09 13:34:28 +0100

    Update hello_world_flask.py

--- hello-world-flask-application/hello_world_flask.py
+++ hello-world-flask-application/hello_world_flask.py
@@ -1,9 +1,17 @@
+
+# import af modulet Flask
 from flask import Flask
+
+# konstruerer en variabel "app" som er af klassen Flask
 app = Flask(__name__)
 
+# En Decorator for en route
 @app.route('/')
-def hello_world():
-    return 'Hello, From Flask!'
 
+# Definer en funktion for vores route der hedder hello_world()
+def hello_world():
+    return 'Hello, From Flask!' # funktionen returnerer en string med beskeden "Hello, from flask"
+
+# til at køre koden direkte
 if __name__== '__main__':
     app.run()
