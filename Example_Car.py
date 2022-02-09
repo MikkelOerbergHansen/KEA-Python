1cf7f0453da08e47c5417fc985c7c3063d8fcb11
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-09 11:49:10 +0100

    Update Example_Car.py

--- Example_Car.py
+++ Example_Car.py
@@ -1,19 +1,65 @@
 # class example
+# Data-Typer:
+# - String = tekst
+# - Boolean = sandt/falsk
+# - Integer = hele tal
+# - Float = decimaltal
+
+
 
 #definer en class
 class Car:
-    def __init__(self, color, brand, model):
+    def __init__(self, color, brand, model, OnSale, MarketPrice, SellingPrice ,KmPerLitre):
         self.color = color                  # definer Attributter
         self.brand = brand
         self.model = model
+        self.OnSale = OnSale
+        self.MarketPrice = MarketPrice
+        self.SellingPrice = SellingPrice
+        self.KmPerLitre = KmPerLitre
 
 
 #definer en variabel af vores Car class
-car1 = Car("Green", "volkswagen", "Golf")
-car2 = Car("Blue", "Toyota", "Prius")
-car3 = Car("Red", "Ford", "Mustang")
+car1 = Car("Green", "volkswagen", "Golf", True, 28000, 24000, 18.1)
+car2 = Car("Blue", "Toyota", "Prius", True, 50000, 40000,15.8 )
+car3 = Car("Red", "Ford", "Mustang", False, 44500, 30000, 16.5 )
 
+#tomme print kald for at skabe mellemrum
+print()
+print()
+
+# en udregning
+car1SalesBonus = (car1.MarketPrice - car1.SellingPrice)*0.20
+car2SalesBonus = car2.MarketPrice - car2.SellingPrice
 #print værdier for valgte attributter på de valgte variable
 print(car1.brand)
 print(car2.brand)
 print(car3.brand)
+
+#tomme print kald for at skabe mellemrum
+print()
+print()
+
+# En lang Print
+print("kære bilsælger")
+print("Velkommen på arbejde")
+print("bilen af mærket " + car1.brand + " i farven " + car1.color + 
+" koster " + str(car1.MarketPrice) + 
+" men du kan sælge den for " + str(car1.SellingPrice))
+
+print("Bonus for salg af Bilen er " + str(car1SalesBonus) + " Kr")
+
+#tomme print kald for at skabe mellemrum
+print()
+print()
+
+#tjek typer af attributter
+print(type(car1))
+print(type(car1.MarketPrice))
+print(type(car1.KmPerLitre))
+print(type(car1.OnSale))
+
+
+#tomme print kald for at skabe mellemrum
+print()
+print()
