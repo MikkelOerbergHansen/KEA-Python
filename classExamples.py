63e30b7b3084b54d87c4500d1910ebb3559702df
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-21 09:35:19 +0100

    Update classExamples.py

---
+++ classExamples.py
@@ -0,0 +1,13 @@
+###
+###  Class on 21/2 2022
+###  Software-konstruktion dag 2
+###  
+###  Vi introducerer user inputs samt loops og if/else statements 
+###
+###
+
+name = input("What is your name? ")
+print("Hello " + name + "!")
+## print med format koden
+print( "Hello {}!".format(name) )
+
