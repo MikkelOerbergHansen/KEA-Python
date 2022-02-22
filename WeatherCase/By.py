b4ffd32bf4ad310d792adefcd7336423b6ef7dff
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-22 08:58:54 +0100

    Update By.py

---
+++ WeatherCase/By.py
@@ -0,0 +1,11 @@
+
+
+class by(object):
+    def __init__(self):
+        self.byen = []
+
+    def addBy(self, VælgBY):
+        self.byen.append(VælgBY)
+
+    def getBy(self, Index):
+        return self.byen[Index]
