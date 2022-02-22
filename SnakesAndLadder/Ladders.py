73b7d525f3034e9aaeff015fada5e0468e0ed1ad
Mikkel Oerberg Hansen <mikkeloerberg@gmail.com>
2022-02-22 01:10:25 +0100

    new classes

---
+++ SnakesAndLadder/Ladders.py
@@ -0,0 +1,12 @@
+
+
+class ladders():
+    def __init__(self):
+        self.ladderIN = []
+        self.ladderOUT = []
+
+    def getLadderIN(self):
+        return self.ladderIN
+
+    def getLadderOUT(self,index):
+        return self.ladderOUT[index]
---
+++ SnakesAndLadder/Snakes.py
@@ -0,0 +1,12 @@
+
+
+class snakes():
+    def __init__(self):
+        self.snakeIN = []
+        self.snakeOUT = []
+
+    def getSnakeIN(self):
+        return self.snakeIN
+
+    def getSnakeOUT(self,index):
+        return self.snakeOUT[index]
