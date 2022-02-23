3b1d3eed8e3a8cfa095e87d9583148ee4fcba0b3
Mikkel Oerberg Hansen <mikkeloerberg@gmail.com>
2022-02-23 22:49:13 +0100

    stuuff

--- VejrCase/VejrStation.py
+++ VejrCase/VejrStation.py
@@ -25,11 +25,12 @@ class VejrStation(object):
     def PrintData2(self):
         header1 = "="*114
         spacing1 = "<br /><br />"
-        header2 = "| {0:20} | {1:15} | {2:20} | {3:12} | {4:13} | {5:15} | ".format(self.måletyper[0],self.måletyper[1], self.måletyper[2], self.måletyper[3], self.måletyper[4], self.måletyper[5])
+        spacing2 = "<br />"
+        header2 = "| {0}{1} | {2:15} | {3:20} | {4:12} | {5:13} | {6:15} | ".format(self.måletyper[0], "&nbsp"*9 , self.måletyper[1], self.måletyper[2], self.måletyper[3], self.måletyper[4], self.måletyper[5])
         textblock = header1+spacing1+header2
         for i in range(0,1409):
-            textblock = textblock + spacing1
-            tempstring = "| {0:20} | {1:15} | {2:20} | {3:12} | {4:13} | {5:15} | ".format(self.målinger[i].getAllMåling()[0], self.målinger[i].getAllMåling()[1], self.målinger[i].getAllMåling()[2], self.målinger[i].getAllMåling()[3], self.målinger[i].getAllMåling()[4], self.målinger[i].getAllMåling()[5])
+            textblock = textblock + spacing2
+            tempstring = "| {0} | {1:15} | {2:20} | {3:12} | {4:13} | {5:15} | ".format(self.målinger[i].getAllMåling()[0], self.målinger[i].getAllMåling()[1], self.målinger[i].getAllMåling()[2], self.målinger[i].getAllMåling()[3], self.målinger[i].getAllMåling()[4], self.målinger[i].getAllMåling()[5])
             textblock = textblock + tempstring
         
         return textblock
