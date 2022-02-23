058294935e0a5401fc73caf5cbb370e8c918d27f
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-23 01:59:38 +0100

    stuff

--- VejrCase/VejrStation.py
+++ VejrCase/VejrStation.py
@@ -19,10 +19,10 @@ class VejrStation(object):
         return self.målinger
 
     def PrintData(self,indexstart, indexslut):
-        print("="*139)
-        print("| {0:20} | {1:20} | {2:20} | {3:20} | {4:20} | {5:20} | ".format(self.måletyper[0],self.måletyper[1], self.måletyper[2], self.måletyper[3], self.måletyper[4], self.måletyper[5]))
+        print("="*114)
+        print("| {0:20} | {1:15} | {2:20} | {3:12} | {4:13} | {5:15} | ".format(self.måletyper[0],self.måletyper[1], self.måletyper[2], self.måletyper[3], self.måletyper[4], self.måletyper[5]))
         for i in range(indexstart, indexslut+1):
-            print("| {0:20} | {1:20} | {2:20} | {3:20} | {4:20} | {5:20} | ".format(self.målinger[i].getAllMåling()[0], self.målinger[i].getAllMåling()[1], self.målinger[i].getAllMåling()[2], self.målinger[i].getAllMåling()[3], self.målinger[i].getAllMåling()[4], self.målinger[i].getAllMåling()[5]))
+            print("| {0:20} | {1:15} | {2:20} | {3:12} | {4:13} | {5:15} | ".format(self.målinger[i].getAllMåling()[0], self.målinger[i].getAllMåling()[1], self.målinger[i].getAllMåling()[2], self.målinger[i].getAllMåling()[3], self.målinger[i].getAllMåling()[4], self.målinger[i].getAllMåling()[5]))
 
 
 
