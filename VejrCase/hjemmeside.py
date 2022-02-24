8a672f8d272b12320ebdd65fed37eeae5001a1a9
Mikkel Oerberg Hansen <mikkeloerberg@gmail.com>
2022-02-24 21:36:26 +0100

    first plot

--- VejrCase/hjemmeside.py
+++ VejrCase/hjemmeside.py
@@ -1,11 +1,17 @@
 # Vi importerer modulet Flask ind i vores Python fil
 from flask import Flask, render_template
 import csv
+
+from matplotlib.axes import Axes
 from ExcelReader import ExcelReader
+from matplotlib import pyplot as plt
+from Målinger import Måling
+from VejrStation import VejrStation
 
 
 # Vi laver en variabel "app" som er en instantiering af klassen Flask
 app = Flask(__name__)
+
 
 # Decorator for en route så den får adressen http://127.0.0.1:5000/
 @app.route('/')
@@ -79,9 +85,6 @@ def Aalborg():
 # Decorator for en route så den får adressen http://127.0.0.1:5000/Grafer
 @app.route('/Grafer')
 def Grafer():
-        ExcelFilData = ExcelReader()
-        dataframe = ExcelFilData.prepareData("VejrCase/VejrDataAalborg.csv")
-        del dataframe[0]
 
         return( '<center><h1>Grafer</h1></center> '
                 '<br /><br /><a href="/Odense"><button>Se Odense</button></a>'
