27dfc18bbc8c776e56cc363ffa7334b7a216ac6b
Mikkel Oerberg Hansen <mikkeloerberg@gmail.com>
2022-02-24 10:37:07 +0100

    stuff

--- VejrCase/hjemmeside.py
+++ VejrCase/hjemmeside.py
@@ -2,8 +2,7 @@
 from flask import Flask, render_template
 import csv
 from ExcelReader import ExcelReader
-from VejrStation import VejrStation
-from Målinger import Måling
+
 
 # Vi laver en variabel "app" som er en instantiering af klassen Flask
 app = Flask(__name__)
@@ -32,26 +31,14 @@ def index():
 def København():
         ExcelFilData = ExcelReader()
         dataframe = ExcelFilData.prepareData("VejrCase/VejrDataKøbenhavn.csv")
-
-        MålingerKBH =[]
-        for i in range(1, len(dataframe)):
-            MålingerKBH.append(Måling(dataframe[i][0],dataframe[i][1],dataframe[i][2],dataframe[i][3],dataframe[i][4], dataframe[i][5]))
-        
-        vejrStation1 = VejrStation("København", "København C", "DK", MålingerKBH)
-        
-        return( 'Vejret i København er '
-                '<br /><br /><a href="/KøbenhavnSenesteDøgn"><button>Se København Seneste Døgn</button></a>'
+        del dataframe[0]
+      
+        return( '<h2>København</h2> '
                 '<br /><br /><a href="/Aalborg"><button>Se Aalborg</button></a>'
                 ' <a href="/Odense"><button>Se Odense</button></a>'
              '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>'
-             '<br /><br />' + render_template('htmlTable.html'))
-        
+             '<br /><br />' + render_template('htmlTable.html', data = dataframe ))
              
-       
-@app.route('/KøbenhavnSenesteDøgn')
-def KøbenhavnSenesteDøgn():
-        return( 'Vejret i København det seneste døgn er '
-             '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>' )       
 
 
 # Decorator for en route så den får adressen http://127.0.0.1:5000/Odense
@@ -59,47 +46,29 @@ def KøbenhavnSenesteDøgn():
 def Odense():
         ExcelFilData = ExcelReader()
         dataframe = ExcelFilData.prepareData("VejrCase/VejrDataOdense.csv")
+        del dataframe[0]
 
-        MålingerOD = []
-        for i in range(1, len(dataframe)):
-            MålingerOD.append(Måling(dataframe[i][0],dataframe[i][1],dataframe[i][2],dataframe[i][3],dataframe[i][4], dataframe[i][5]))
-        
-        vejrStation2 = VejrStation("Odense", "Odense C", "DK", MålingerOD)
-        
-        return( 'Vejret i Odense er '
-            '<br /><br /><a href="/OdenseSenesteDøgn"><button>Se Odense Seneste Døgn</button></a>'
-            '<br /><br /><a href="/København"><button>Se København</button></a>'
-            ' <a href="/Aalborg"><button>Se Aalborg</button></a>'
-           '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>' )           
+      
+        return( '<h2>Odense</h2> '
+                '<br /><br /><a href="/Aalborg"><button>Se Aalborg</button></a>'
+                ' <a href="/København"><button>Se København</button></a>'
+             '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>'
+             '<br /><br />' + render_template('htmlTable.html', data = dataframe ))          
 
-@app.route('/OdenseSenesteDøgn')
-def OdenseSenesteDøgn():
-        return( 'Vejret i Odense det seneste døgn er '
-             '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>' )
+
 
 # Decorator for en route så den får adressen http://127.0.0.1:5000/Aalborg
 @app.route('/Aalborg')
 def Aalborg():
         ExcelFilData = ExcelReader()
         dataframe = ExcelFilData.prepareData("VejrCase/VejrDataAalborg.csv")
+        del dataframe[0]
 
-        MålingerAA =[]
-        for i in range(1, len(dataframe)):
-            MålingerAA.append(Måling(dataframe[i][0],dataframe[i][1],dataframe[i][2],dataframe[i][3],dataframe[i][4], dataframe[i][5]))
-        
-        vejrStation3 = VejrStation("Aalborg", "Aalborg C", "DK", MålingerAA)
-
-
-        return( 'Vejret i Aalborg er '
-            '<br /><br /><a href="/AalborgSenesteDøgn"><button>Se Aalborg Seneste Døgn</button></a>'
-            '<br /><br /><a href="/København"><button>Se København</button></a>'
-            ' <a href="/Odense"><button>Se Odense</button></a>'
-           '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>' )
-
-@app.route('/AalborgSenesteDøgn')
-def AalborgSenesteDøgn():
-        return( 'Vejret i Aalborg det seneste døgn er '
-             '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>' )
+        return( '<h2>Aalborg</h2> '
+                '<br /><br /><a href="/Odense"><button>Se Odense</button></a>'
+                ' <a href="/København"><button>Se København</button></a>'
+             '<br /><br /><a href="/"><button>Tilbage til forsiden</button></a>'
+             '<br /><br />' + render_template('htmlTable.html', data = dataframe )) 
 
 # til at køre koden direkte
 if __name__== '__main__':
--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-Pokemons =["Pikachu", "Charizard", "Squirtle"]
+Pokemons =[["Pikachu", "Charizard", "Squirtle"], ["sdfækgæa", "aszdkjæzg", "zldkvl"]]
 
 
 @app.route('/')
 def index():
-   return render_template("htmlTable.html", len = len(Pokemons), Pokemons = Pokemons) #if you want to render a .html file,
+   return render_template("htmlTable.html", Pokemons = Pokemons) #if you want to render a .html file,
                         # import render_template from flask and use
                         #render_template("index.html") here.
 
