0ddb30cf40092ef9d90ea48325e35b780d228644
Mikkel Oerberg Hansen <mikkeloerberg@gmail.com>
2022-02-27 03:51:15 +0100

    543

--- Shop_example/flask_code.py
+++ Shop_example/flask_code.py
@@ -10,7 +10,18 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     processed_text = text.upper()
-    return processed_text
+    text2 = request.form['text2']
+    return ('welcome ' + processed_text + '<br /><br />' +
+            'your password is ' + text2 + '<br /><br />' + 
+            '<a href="/salesPage"><button>start shopping</button></a>')
+
+
+@app.route('/salesPage')
+def salesPage():
+    return ('welcome to the shooping page')
+
+
+
 
 if __name__ == "__main__":
     app.run()
