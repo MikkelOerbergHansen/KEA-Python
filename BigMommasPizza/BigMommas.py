64a6ec8d3581c7222d910c520fd0619dcb35cef1
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-03-10 13:53:51 +0100

    Update BigMommas.py

--- BigMommasPizza/BigMommas.py
+++ BigMommasPizza/BigMommas.py
@@ -2,25 +2,32 @@ from TakeOrder import TakeOrder
 from MenuKort import MenuKort
 
 
-print( "\nVelkommen Til Big Mommas Pizza !!! \n")
 
 Menu = MenuKort()
-Menu.MakeMenuKort()
+#Menu.MakeMenuKort()
 Menu.AddPizza("Meat Lovers Pizza",["skinke","pepperoni","kebab","cocktail-Pølser"], 80)
-
-print("Vil du se MenuKortet ? \n")
-BrugerSvar = input("y = yes eller n = no\n>>>> " )
-tempSvar = False
-while tempSvar == False:
-    if BrugerSvar == "y":
-        Menu.ShowMenuKort()
-        tempSvar = True
-    elif BrugerSvar == "n":
-        print("\nVi er Klar til at tage din ordre")
-        tempSvar = True
-    else:
-        print("\nDet Forstår vi ikke")
-        BrugerSvar = input("y = yes eller n = no\n>>>> ")
-        tempSvar = False
+Menu.ShowMenuKort()
 
 
+
+
+
+def testrun():
+    print( "\nVelkommen Til Big Mommas Pizza !!! \n")
+    print("Vil du se MenuKortet ? \n")
+    BrugerSvar = input("y = yes eller n = no\n>>>> " )
+    tempSvar = False
+    while tempSvar == False:
+        if BrugerSvar == "y":
+            Menu.ShowMenuKort()
+            tempSvar = True
+        elif BrugerSvar == "n":
+            print("\nVi er Klar til at tage din ordre")
+            tempSvar = True
+        else:
+            print("\nDet Forstår vi ikke")
+            BrugerSvar = input("y = yes eller n = no\n>>>> ")
+            tempSvar = False
+
+
+#testrun()
