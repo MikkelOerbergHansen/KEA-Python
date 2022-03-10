3d8a8515d8868d9b04ea358dc6cac297cf2d8307
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-03-10 19:44:43 +0100

    edgfh

--- BigMommasPizza/TakeOrder.py
+++ BigMommasPizza/TakeOrder.py
@@ -23,10 +23,16 @@ class TakeOrder(object):
             PizzaString = PizzaString + "\n" + " {0:40} kr: {1:2} ".format(pizza.getName() , pizza.getPrice()) 
         PizzaString = PizzaString + "\n" + "========================================================"
 
-        return str("Order number: " + str(self.OrderId) + "\n" +
+        return str("****************************************************************************************************\n" +
+                    "{0:30} {1:22} {2:30} ".format("", "BIG MOMMAS GASTRONOMIA", "") + "\n" +
+                    "{0:32} {1:22} {2:30} ".format("", "Frederikværksgade 4", "") + "\n" +
+                    "{0:35} {1:22} {2:30} ".format("", "3400 Hillerød", "") + "\n" +
+                    "{0:33} {1:22} {2:30} ".format("", "tlf: 40 12 40 03", "") + "\n" +
+                    "Order number: " + str(self.OrderId) + "\n" +
                     "for " + self.name + "\n" + 
                     "takeAway: " + str(self.ToGo) + "\n" + 
                     PizzaString + "\n" + 
-                    "Total Price: " + "{0:33}{1:4}Kr".format("",str(self.TotalPrice)) + "\n")
+                    "Total Price: " + "{0:33}{1:4}Kr".format("",str(self.TotalPrice)) + "\n" + 
+                    "****************************************************************************************************")
 
 
