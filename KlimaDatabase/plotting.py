3e1d38f4c0413d13f33dcfa2c778e95413695591
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-03-15 00:11:24 +0100

    kljhk

--- KlimaDatabase/plotting.py
+++ KlimaDatabase/plotting.py
@@ -1,11 +1,12 @@
+
+
+
 import pandas as pd
 
+# open the file
+xlsx = pd.read_excel("KlimaDatabase/static/Results_FINAL_20210201v4.xlsx", engine = "openpyxl", sheet_name = "Ra_500food")
 
-### indlæs excel fil
 
-xl = pd.ExcelFile("KLimaDatabase/static/Results_FINAL_20210201v4.xlsx")
-
-xl.sheet_names
 
 
 
