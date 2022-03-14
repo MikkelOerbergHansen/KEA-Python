c7f197fc2349950804afb1cc77b7d1ae7d415665
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-03-15 00:19:25 +0100

    Update plotting.py

--- KlimaDatabase/plotting.py
+++ KlimaDatabase/plotting.py
@@ -7,6 +7,8 @@ import pandas as pd
 xlsx = pd.read_excel("KlimaDatabase/static/Results_FINAL_20210201v4.xlsx", engine = "openpyxl", sheet_name = "Ra_500food")
 
 
+df = pd.DataFrame(xlsx, columns= ['DSK Kategori'])[1:501]   # enkelt søjle
+print (df)
 
 
 
