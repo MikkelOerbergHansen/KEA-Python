ac0816f99b4fe4c1a1043ea203ab82c376e9de84
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-03-14 23:46:52 +0100

    Update plotting.py

--- KlimaDatabase/plotting.py
+++ KlimaDatabase/plotting.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 ### indlæs excel fil
 
-xl = pd.ExcelFile("static/Results_FINAL_20210201v4.xlsx")
+xl = pd.ExcelFile("KLimaDatabase/static/Results_FINAL_20210201v4.xlsx")
 
 xl.sheet_names
 
