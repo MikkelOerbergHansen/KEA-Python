0f88274fb7bfbc53fa3e4db6526073b1c31c1952
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-04-29 10:13:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
+#Step 1: Open up a terminal and run pip install pyinstaller
+#Step 2: Using the terminal, go to the directory where your script is located (use the cd command)
+#Step 3: Once you‘re in the right directory, write a command with the following syntax
+#        pyinstaller --onefile name_of_script.py in the terminal to make the script executable.
+
+
+
+
+
+
+
 
 ## auto-py-to-exe
 ## https://towardsdatascience.com/how-to-easily-convert-a-python-script-to-an-executable-file-exe-4966e253c7e9
