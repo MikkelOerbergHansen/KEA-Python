052b994c9468c62d38215641ea62adece8a32d8a
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-05-18 13:56:59 +0200

    Create snippets3.py

---
+++ Bil_Example/snippets3.py
@@ -0,0 +1,60 @@
+
+
+
+## titelside : navnet påjeres it-system
+# "udviklingsprojekt for BLABLA system"
+
+
+## abstract : skrive kort om det endelige system 
+# kan også godt skrive noget om de metoder der er anvendt.add()
+# 
+
+
+## indholdsfortegnelse : hvad skal underkapitler være?
+# hvordan passer it ind?
+
+
+## Introduktion :
+# introduccere udviklingen af et IT-system
+# systemet har et formål for virksomheden/ og eller verden, og hvorfor er det relevant at løse dette formål. 
+# årsrpport og pressemeddelser er gode kilder til at forstå formål
+
+## problemformulering : Designe og implementere systemet til ....(case)
+# vi bygger et system der ikke findes i forvejen. 
+
+## metode : 
+# OOP - systemet udvikles med OOP i tankerne. - introducer
+# kravspecifikation og dennes udvikling. 
+# systemet designes med UML teknik. - introducer 
+# inception deck - introducer metoden og hvordan det gribes an. 
+# nævn waterfall / iterativt - hvordan vi valgte at arbejde undervejs i processen. 
+#
+
+## analyse :
+# design og implementering af BLABLA system
+# - et afsnit om inception deck
+# - et afsnit med uml diagrammer
+# - et afsnit med code snippets. 
+# - screendumps af prototype. 
+
+## diskussion :
+# - diskuter arbejdsprocessen waterfall/iterativt hvordan er de forskellige fordele/ulemper
+# - hvilke udfordringer har der været (program tekniske)
+# - sammenhæng mellem uml diagram og koden. 
+# - er der moduler der kunne hjælpe i udviklingen. 
+
+## konklusion : 
+# hvad er det for et system vi har fået udviklet.
+# IT-systemet er designet og implementeret med metoderne BLA BLA BLA 
+# systemet kan Bla bla bla. 
+
+## bilag
+# der lan lægges fulde inception deck
+# grafer
+# vedlæg python filer som ekstra materiale. 
+
+## persppektivering :
+# hvad skal der ske med programmet fremadrettet, kan man øge funktionalitetetn , gøre det mere tilgængeligt?
+# hvis ikke flask så måske perspektiver til hvordan flask
+# hvis flask så måske hvordan selve launch fungerer (cloud osv.)
+# relevans af system for case-virksomheden. 
