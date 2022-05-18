86d7f8f0bfb8759ce751c5401f1a5e27ad45f1a4
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-05-18 10:01:08 +0200

    34

---
+++ NY-mappe/snippets.py
@@ -0,0 +1,40 @@
+##
+## Vi definerer en klasse Car og instantierer objekter af klassen Car
+
+## Defineringen skal indeholde attributter og metoder
+
+## Attributter:
+# - beskrivelse af bilen
+# - vægt, farve, brændstof, pris, mv. 
+# - tager udgangspunkt i forskellige data typer.  (str, int, float, bool)
+# - eller datakollektioner (lister, dictionary)
+# - attributter kan også være objekter fra andre klasser. 
+
+
+## Metoder:
+# - hvad bilen kan eller gør (adfærd)
+# - kan være udregninger af forskellige art
+# - ting til at redigere de ovennævnte attributter. (get / set funktioner)
+# - for at konstruere metoder bruges ofte:
+# - if / elif / else statements som eksekverer kode hvis en betingelse er oplyst. 
+# - for loops / andre loops. (benytter en betingelse og itererer gennem liste)
+
+
+## fejlhåndtering:
+# - try / except blok
+
+## filhåndtering:
+# - csv og pandas (indlæs moduler)
+
+## graftegning til data
+# - matplotlib og pandas (indlæs moduler)
+
+
+
+
+
+
+
+
+
+
