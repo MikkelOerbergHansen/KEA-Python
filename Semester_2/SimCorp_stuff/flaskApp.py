b77d0ef0d4d3f3528a68cd75565c30e7c8e749cc
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-11-24 10:18:35 +0100

    hggf

---
+++ Semester_2/SimCorp_stuff/flaskApp.py
@@ -0,0 +1,55 @@
+
+# import af modulet Flask
+from flask import Flask, request, render_template, redirect, session
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def startside():
+    HeadLine = "SimCorp"
+    return render_template('Startside.html', Headline = HeadLine)
+
+
+
+
+
+
+
+
+# til at køre koden direkte
+if __name__== '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
