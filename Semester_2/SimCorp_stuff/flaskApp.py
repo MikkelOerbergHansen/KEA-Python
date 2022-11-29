3b14c906a931ff52204a5be02122e39d742a1e61
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-11-29 02:24:03 +0100

    vb

--- Semester_2/SimCorp_stuff/flaskApp.py
+++ Semester_2/SimCorp_stuff/flaskApp.py
@@ -8,26 +8,25 @@ import matplotlib.pyplot as plt
 
 
 
+def TreeExample():
+    df = pandas.read_excel("Semester_2/SimCorp_stuff/Static/testData.xlsx", sheet_name="DecTree")
 
-df = pandas.read_csv("data.csv", sheet_name="DecTree")
+    d = {'UK': 0, 'USA': 1, 'N': 2}
+    df['Nationality'] = df['Nationality'].map(d)
+    d = {'YES': 1, 'NO': 0}
+    df['Go'] = df['Go'].map(d)
 
-d = {'UK': 0, 'USA': 1, 'N': 2}
-df['Nationality'] = df['Nationality'].map(d)
-d = {'YES': 1, 'NO': 0}
-df['Go'] = df['Go'].map(d)
+    features = ['Age', 'Experience', 'Rank', 'Nationality']
 
-features = ['Age', 'Experience', 'Rank', 'Nationality']
+    X = df[features]
+    y = df['Go']
 
-X = df[features]
-y = df['Go']
+    dtree = DecisionTreeClassifier()
+    dtree = dtree.fit(X, y)
 
-dtree = DecisionTreeClassifier()
-dtree = dtree.fit(X, y)
-
-tree.plot_tree(dtree, feature_names=features)
-
-
-
+    tree.plot_tree(dtree, feature_names=features)
+    plt.savefig('Semester_2/SimCorp_stuff/Static//Treeplot.png')
+    return df
 
 
 
@@ -73,15 +72,18 @@ def LinearRegEx():
 @app.route('/DecTreeExample', methods=['GET', 'POST'])
 def DecTreesEx():
     HeadLine = "Decision Tree"
+    TreeExample()
+    rule1 = "convert the values 'UK' to 0, 'USA' to 1, and 'N' to 2"
+    rule2 = "convert the values 'YES' to 1, 'NO' to 0"
 
     if request.method == 'POST':
         SearchWord = request.form['searchbar']
         
-        return render_template('DecTree.html', Headline = HeadLine, Search = SearchWord)
+        return render_template('DecTree.html', Headline = HeadLine, Search = SearchWord, rule1=rule1, rule2=rule2)
 
 
     
-    return render_template('DecTree.html', Headline = HeadLine)
+    return render_template('DecTree.html', Headline = HeadLine, rule1=rule1, rule2=rule2)
 
 
 
