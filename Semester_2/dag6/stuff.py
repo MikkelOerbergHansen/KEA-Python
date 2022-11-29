5ba7932bbde864e7cc9044eee49b68f31f9d316b
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-11-29 13:10:11 +0100

    uy

---
+++ Semester_2/dag6/stuff.py
@@ -0,0 +1,130 @@
+# import af modulet Flask
+from flask import Flask, request, render_template, redirect, session, jsonify
+
+import requests
+import json
+
+# 
+
+def elspotpriser():
+    #url = 'https://api.energidataservice.dk/dataset/Elspotprices?offset=0&sort=HourUTC%20DESC&timezone=dk'
+    url = 'https://api.energidataservice.dk/dataset/Elspotprices?offset=0&filter=%7B%22PriceArea%22:[%22DK1%22]%7D&sort=HourUTC%20DESC&timezone=dk'
+    headers = {"Content-Type": "application/json"}
+    response = requests.get(url, headers=headers).json()
+    #print(response)
+    with open('Semester_2/dag6/price_data.json', 'w') as f:
+        json.dump(response, f)
+
+    return response
+
+def getTimedata(data):
+    tidspunkter = []
+
+    for x in range(0,len(data["records"])):
+        if data["records"][x]["PriceArea"] == "DK1":
+            tidspunkter.append(data["records"][x]["HourDK"])
+    return tidspunkter
+
+def getPriceData(data):
+    priser = []
+
+    for x in range(0,len(data["records"])):
+        if data["records"][x]["PriceArea"] == "DK1":
+            priser.append(data["records"][x]["SpotPriceDKK"])
+    return priser
+
+
+
+'''
+elpriser = elspotpriser()
+
+timeslots = getTimedata(elpriser)
+priser = getPriceData(elpriser)
+
+print(len(timeslots))
+
+print(len(priser))
+
+print(timeslots[0].split("T")[1].split(":", 2)[0])
+print(timeslots[0].split("T")[1].split(":", 2)[0])
+'''
+
+app = Flask(__name__)
+
+
+@app.route('/' , methods=['GET', 'POST'])
+def startside():
+    
+    
+
+    if request.method == 'POST':
+        
+        input1 = request.form['input1']
+        input2 = request.form['input2']
+        
+        elpriser = elspotpriser()
+               
+        timeslots = getTimedata(elpriser)
+        priser = getPriceData(elpriser)
+        mydata = []
+        
+        for i in range(0,len(timeslots)):
+            if int(input1.split(":")[0]) <= int(timeslots[i].split("T")[1].split(":", 2)[0]) and int(input2.split(":")[0]) >= int(timeslots[i].split("T")[1].split(":", 2)[0]): 
+                tempdata = []
+                tempdata.append(timeslots[i])
+                tempdata.append(priser[i])
+                mydata.append(tempdata)
+
+        return render_template('index.html', myinput1=input1, myinput2=input2, data = mydata)
+    
+    return render_template('index.html')
+
+
+
+
+@app.route('/api/data' , methods=['GET'])
+def APIside():
+    data = elspotpriser()
+
+    return jsonify(data)
+
+
+
+
+
+
+
+# til at køre koden direkte
+if __name__== '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
