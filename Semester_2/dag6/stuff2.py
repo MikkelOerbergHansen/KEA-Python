bfefba19739c1fa3f9fedea0d1b03e242b0efa0b
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-11-29 14:20:04 +0100

    Update stuff2.py

--- Semester_2/dag6/stuff2.py
+++ Semester_2/dag6/stuff2.py
@@ -9,10 +9,7 @@ def elspotpriser():
     url = 'http://127.0.0.1:5000/api/data'
     headers = {"Content-Type": "application/json"}
     response = requests.get(url, headers=headers).json()
-    #print(response)
-    with open('Semester_2/dag6/price_data.json', 'w') as f:
-        json.dump(response, f)
-
+    
     return response
 
 
