39bbdd61cfa76a0a1e97402e11cbef479fd3ef0d
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-12-08 13:10:37 +0100

    Update stuff.py

--- Semester_2/recap/stuff.py
+++ Semester_2/recap/stuff.py
@@ -1,4 +1,36 @@
 
+import yfinance as yf
+
+
+
+msft = yf.Ticker("SIM.CO")
+
+print(msft.info)
+
+hist=msft.history(period="max")
+
+print(msft.actions)
+
+print(msft.dividends)
+
+print(msft.splits)
+
+print(msft.shares)
+
+#print(msft.income_stmt)
+
+#print(msft.quarterly_income_stmt)
+
+print(msft.balance_sheet)
+
+#print(msft.quarterly_balance_stmt)
+
+
+print(msft.cashflow)
+
+print(msft.quarterly_cashflow)
+
+print(msft.major_holders)
 
 
 
